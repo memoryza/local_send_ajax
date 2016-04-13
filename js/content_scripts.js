48831567d6063962c819808650ead3dc432e57c0
memoryza <jincai.wang@foxmail.com>
2016-04-13 13:53:33 +0800

    删除search中参数确定

--- js/content_scripts.js
+++ js/content_scripts.js
@@ -1,10 +1,8 @@
 var bnjsUri = chrome.extension.getURL('js/http.js?time=' + (+new Date()));
 
-if (window.location.search.indexOf('bnjs_simulator') > -1) {
-    var script = document.createElement('script');
-    script.type = 'text/javascript';
+var script = document.createElement('script');
+script.type = 'text/javascript';
 
-    script.src = bnjsUri;
+script.src = bnjsUri;
 
-    document.head.appendChild(script);
-}
+document.head.appendChild(script);
